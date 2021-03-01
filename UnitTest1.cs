5b1794c1a71787e62a9499c535d280cf2df10329
VinokurovValeriy <v.vinokurov@qa-academy.by>
2021-03-01 20:59:32 +0300

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestCookie
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
